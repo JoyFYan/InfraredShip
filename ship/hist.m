5a2aae779cc4cd4f93fe5c79adde77c2f2ad65cb
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-09-14 10:28:57 +0800

    ship photos
    infrared photos

---
+++ ship/hist.m
@@ -0,0 +1,12 @@
+clear all;close all;clc;
+I=imread('1.bmp');
+J=rgb2gray(I);
+figure;
+imshow(J);
+hist_im=imhist(J);
+figure ;
+bar(hist_im);
+K=J(1:120,:);
+hist_im=imhist(K);
+figure ;
+bar(hist_im);
