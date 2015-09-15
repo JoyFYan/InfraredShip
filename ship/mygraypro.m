d98439ed86140941dc7b0017b405ae213eda5d7f
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-09-15 23:19:41 +0800

    增加拟合曲线
    增加拟合曲线对比,下一步:对曲线求极大值,极小值,间距比例作为评判依据

--- ship/mygraypro.m
+++ ship/mygraypro.m
@@ -7,6 +7,7 @@ end
 if v==3
     I=rgb2gray(I);
 end
+x0=(1:256)';
 all=m*n;
 [con,x]=imhist(I);
 conre=con/all;
@@ -15,19 +16,22 @@ subplot(1,3,1)
 imshow(I);
 subplot(1,3,2)
 bar(x,con);
-
+mycreateFit(x0, conre);
 
 K=uint8(zeros(30,320,8));
 figure(2)
 for i=1:8
     K(:,:,i)=I(((1+(i-1)*30):i*30),:);
     temphist=imhist(K(:,:,i));
+    figure(2)
     subplot(4,4,i)
     imshow(K(:,:,i));
+    figure(2)
     subplot(4,4,i+8)
     bar(temphist)
     
     temphistre=temphist/30/320;
+    mycreateFit(x0, temphistre);
     tempsub=conre-temphistre;
     subres(i)=sum(abs(tempsub));
 end
