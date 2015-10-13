b007d8a7dd58690f3199d4c73df9a48f46aeac73
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-10-13 13:01:52 +0800

    增加阈值系数k

--- ship/background.m
+++ ship/background.m
@@ -1,12 +1,16 @@
 clear;close all;clc;
-I=imread('4.bmp');
+k=1;
+I=imread('10.bmp');
 J=rgb2gray(I);
 [m n]=size(J);
 figure;
 imshow(J);
-thresh = graythresh(J);     %自动确定二值化阈值
+hist_im=imhist(J);
+figure ;
+bar(hist_im);
+thresh = 255*graythresh(J);     %自动确定二值化阈值
 %I2 = im2bw(J,thresh);
-I2 = J-255*thresh*uint8(ones(m,n));
+I2 = J-k*thresh*uint8(ones(m,n));
 figure
 imshow(I2);
 max=max(max(I2))
