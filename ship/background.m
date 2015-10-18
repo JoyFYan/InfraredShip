19639a99887e511e62d98d8ab18951071dcfd1d3
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-10-18 21:51:17 +0800

    增加实验过程中数据

--- ship/background.m
+++ ship/background.m
@@ -1,11 +1,12 @@
 clear;close all;clc;
 k=1;
-I=imread('3.bmp');
+I=imread('4.bmp');
 J=rgb2gray(I);
 [m, n]=size(J);
 figure(1);
 subplot(221)
 imshow(J);
+title('原始图像')
 hist_im=imhist(J);
 % subplot(222)
 % bar(hist_im);
@@ -13,17 +14,22 @@ thresh = 255*graythresh(J);     %自动确定二值化阈值
 %I2 = im2bw(J,thresh);
 I2 = J-k*thresh*uint8(ones(m,n));
 subplot(222)
+
 imshow(I2);
+title('自适应阈值')
 max=max(max(I2));
 I3=int16(I2)*255/int16(max);
 
 
 subplot(223)
 I3=uint8(I3);
+
 imshow(I3);
+title('提高对比度')
 subplot(224)
 h=sum(I3,2);
 bar(h)
+title('横向灰度和')
 
 figure(2)
 subplot(321)
@@ -42,12 +48,13 @@ fd=fftshift(fft2(I3));
  %imshow(H)
  fresult=H.*fd;
  imshow(abs(log(1+fresult)),[ ]);
+ title('高通滤波器')
   FS=H.*fresult;
 
 fr=real(ifft2(ifftshift(FS)));
 ret=im2uint8(mat2gray(fr));
 subplot(322),imshow(ret);
-
+title('结果')
 % thresh = graythresh(ret);     %自动确定二值化阈值
 % I2 = im2bw(ret,thresh);
 % figure(3)
@@ -70,12 +77,13 @@ fd=fftshift(fft2(I3));
  %imshow(H)
  fresult=H.*fd;
  imshow(abs(log(1+fresult)),[ ]);
-  FS=H.*fresult;
+ title('高通滤波器（横向椭圆）')
+ FS=H.*fresult;
 
 fr=real(ifft2(ifftshift(FS)));
 ret=im2uint8(mat2gray(fr));
 subplot(324),imshow(ret);
-
+ title('结果')
 subplot(325)
 fd=fftshift(fft2(I3));
 %imshow(abs(log(1+fd)),[ ]);
@@ -92,8 +100,10 @@ fd=fftshift(fft2(I3));
  %imshow(H)
  fresult=H.*fd;
  imshow(abs(log(1+fresult)),[ ]);
+ title('高通滤波器（纵向椭圆）')
   FS=H.*fresult;
 
 fr=real(ifft2(ifftshift(FS)));
 ret=im2uint8(mat2gray(fr));
 subplot(326),imshow(ret);
+title('结果')
