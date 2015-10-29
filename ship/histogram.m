394ef63277058847b7755c12e2424475d64303de
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-10-29 23:18:34 +0800

    增加图片

--- ship/histogram.m
+++ ship/histogram.m
@@ -1,7 +1,8 @@
 clc ;close all;clear;
-I=imread('lowd.png');%读取图片
+I=imread('8.bmp');%读取图片
 tempmax=0;%找最大值临时变量
 pos=0;%此时灰度位置
+khist=0.5;%均衡程度系数
 J=rgb2gray(I);%灰度化
 I1=histeq(J);%自带均衡
 figure(1);
@@ -48,7 +49,7 @@ for i=locMp:locNp
        pos=i;
    end
 end
-const1=log( (pos/2+1))/pos;%计算指数系数
+const1=log( (pos/khist+1))/pos;%计算指数系数
 const2=9/(255-pos);
 const3=1-pos*const2;
 J0=double(J);
@@ -57,7 +58,7 @@ for i=1:m
         if J0(i,j)<=pos
             J0(i,j)=floor(exp((J0(i,j)*const1))-1);
         else 
-           J0(i,j)=pos/2+(255-pos/2)*log10((const2*J0(i,j)+const3));
+           J0(i,j)=pos/khist+(255-pos/khist)*log10((const2*J0(i,j)+const3));
         end
     end
 end
@@ -69,3 +70,4 @@ h0=imhist(ret);%计算直方图
 subplot(326)
 bar(h0)%绘制直方图
 title('灰度直方图结果')
+[g,~]=find(h0(1:floor(pos/khist))~=0);%不为0的序列
