465b56841cd98096340302bf790f74dd3424a7a3
JoyFreemanYan <JoyF.Yan@gmail.com>
2015-11-02 11:36:24 +0800

    hwmean增加形态学操作
    增加腐蚀等操作

--- ship/gradient.m
+++ ship/gradient.m
@@ -1,45 +1,52 @@
 clc ;close all;clear;
-I=imread('ship.png');%¶ÁÈ¡Í¼Æ¬
-
+I=imread('gra.png');%¶ÁÈ¡Í¼Æ¬
+lamda=2;
 J=rgb2gray(I);%»Ò¶È»¯
 
-J=myhistf( J,1,500 );
-A=fspecial('average',8);
-J=filter2(A,J,'same');
-%J=medfilt2(J,[5 5]);
-[m,n]=size(J);
+I=myhistf( J,1,500 );
 
-Sy=[-1,-1,-1;0,0,0;1,1,1];
+[m,n]=size(I);
+
+Sy=[-1,-1,-1;1,1,1];
 Sx=Sy';
+Syn=[1,1,1;-1,-1,-1];
+Sxn=Sy';
 
-D1=conv2(double(I),double(Sy),'same');
-D2=conv2(double(I),double(Sx),'same');
-D=zeros(m,n);
+Dyp=abs(conv2(double(I),double(Sy),'same'));
+Dxp=abs(conv2(double(I),double(Sx),'same'));
+% Dyn=conv2(double(I),double(Syn));
+% Dxn=conv2(double(I),double(Sxn));
+
+
 %D=sqrt(D1.*D1+D2.*D2);
-D=max(D1,D2);
-figure,imshow(D,[]);
-%t=max(max(D));
-a=sum(sum(D))/m/n;
-th=0;
-for i=1:m
-    for j=1:n
-        th=th+double((D(i,j)-a)^2);
-    end
-end
-R=zeros(m,n);
-deta=sqrt(th/(m*n));
-th=sqrt(th/(m*n))*sqrt(2*log(m*n));
+%D=max(abs(Dyp),abs(Dxp));
+avrx=mean(mean(Dxp(3:m-2,3:n-2)));
+stdx=std(std(Dxp(3:m-2,3:n-2)));
+avry=mean(mean(Dyp(3:m-2,3:n-2)));
+stdy=std(std(Dyp(3:m-2,3:n-2)));
+thx=avrx-lamda*stdx;
+thy=avry-lamda*stdy;
+Dx=zeros(m,n);
+Dx(Dxp>thx)=1;
+Dy=zeros(m,n);
+Dy(Dxp>thy)=1;
+C=and(Dx,Dy);
+%Dx(Dxp>thx)=0;
 % for i=1:m
 %     for j=1:n
-%         if(R(i,j)<th)
-%             R(i,j)=0;
-%         end
-%         if(R(i,j)>th)
-%             R(i,j)=255;
-%         end
+%         if(D(i,j)>avrxm)
 %     end
-%     
 % end
-R=(D>th);
-figure,
-imshow(R,[])
+figure
+subplot(211)
+imshow(Dx)
+subplot(212)
+imshow(Dy)
+
+
+% figure,imshow(Dx,[]);
+% %t=max(max(D));
+% ret=im2uint8(mat2gray(Dx));
+% level = graythresh(ret);
+% i2=im2bw(ret,0.07);
+
